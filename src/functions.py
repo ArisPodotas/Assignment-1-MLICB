0a9298aeec86715d13ee94f0aea7f96389fba4fe
Aris Podotas <a.Podotas@gmail.com>
2025-03-14 18:36:37 +0200

    I'm now sure I'm done with thte data exploration notebook  and I am moving to the model analysis notebook

--- src/functions.py
+++ src/functions.py
@@ -1,14 +1,38 @@
-import argparse as arg
+import pandas as pd
+from collections.abc import Callable
+from time import time
 
-def liner() -> arg.Namespace:
-    """Prses the command line input"""
-    prs = arg.ArgumentParser()
-    cmd = prs.parse_args()
-    return cmd
+def timeit(func: Callable) -> Callable:
+	"""Prints the time a function took to execute"""
+	def wrapper(*args, **kwargs):
+		start= time()
+		result = func(*args, **kwargs)
+		end = time()
+		elapsed = end - start
+		print(f"Function '{func.__name__}' executed in {elapsed:.4f} seconds")
+		return result
+	return wrapper
+
+@timeit
+def dataSplit(dataframe: pd.DataFrame) -> tuple:
+	"""Splits the dataframe to data (x's and y's together) and metadata (id, sex...)"""
+	data = dataframe.iloc[:, 4:]
+	metadata = dataframe.iloc[:, 0:4]
+	return data, metadata
+
+@timeit
+def isolator(data: pd.DataFrame) -> tuple:
+	"""Isolates the x's and y's of the dataframe into different variables"""
+	x = data.iloc[:, 1:]
+	y = data['BMI']
+	return x, y
 
 def main():
-    pass
+	"""Checks that things work"""
+	dev = pd.read_csv('../data/development_final_data.csv')
+	devData, devMeta = dataSplit(dev)
+	train, preds = isolator(devData)
 
 if __name__ == "__main__":
-    main()
+	main()
 
