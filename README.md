7c53221e6d64569531bf6bf2f5d3fa75ae8a21d1
Aris Podotas <a.Podotas@gmail.com>
2025-03-15 11:06:36 +0200

    I'm getting some sort of indexing error when I try to pu all the results into a list that I'm going to solve soon. Began implementing the report file since if I don't I'll forget to do so (what to write) later.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Code style
+
+This section refer to my way of naming, using and implementing things in code.
+
--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,17 @@
+# You will notice that my variable names inside function tend to be
+# different from the ones I use otherwise style wise. I do this because
+# the locality of a function allows for reuse of variable names and I
+# really like small simple variable names (you can probably tell)
+
+from typing import Sequence
 import pandas as pd
+import numpy as np
 from collections.abc import Callable
 from time import time
 
 def timeit(func: Callable) -> Callable:
 	"""Prints the time a function took to execute"""
+	# This is my favorite functions and if you look at my other repositories on github it becomes quite obvious
 	def wrapper(*args, **kwargs):
 		start= time()
 		result = func(*args, **kwargs)
@@ -16,6 +24,9 @@ def timeit(func: Callable) -> Callable:
 @timeit
 def dataSplit(dataframe: pd.DataFrame) -> tuple:
 	"""Splits the dataframe to data (x's and y's together) and metadata (id, sex...)"""
+	# I use hard coded metrics because we have been informed about the format
+	# If the format was permiable I'd make this take input and somehow determine it
+	# in another function I think
 	data = dataframe.iloc[:, 4:]
 	metadata = dataframe.iloc[:, 0:4]
 	return data, metadata
@@ -23,9 +34,26 @@ def dataSplit(dataframe: pd.DataFrame) -> tuple:
 @timeit
 def isolator(data: pd.DataFrame) -> tuple:
 	"""Isolates the x's and y's of the dataframe into different variables"""
-	x = data.iloc[:, 1:]
+	x = data.iloc[:, 1:] # everything other than the BMI
 	y = data['BMI']
 	return x, y
+
+@timeit
+def regression(method: Callable, train: pd.DataFrame, pred: pd.DataFrame, val: pd.DataFrame) -> np.ndarray:
+	"""Calls the method on the data given and return outputs"""
+	object = method()
+	object.fit(train, pred)
+	return object.predict(val)
+
+@timeit
+def applyMetrics(metrics: Sequence[Callable], truth: np.ndarray, data: np.ndarray) -> np.ndarray:
+	"""Applies all the given metrics to the data assuming as ground truth the truth given"""
+	# Note that the function takes input as numpy array and not a dataframe which is why we convert the input in the notebook in the ()'s of the function
+	# Holder of the outputs
+	output = np.array([0] * len(metrics))
+	for i, metric in enumerate(metrics):
+		output[i] = metric(truth, data)
+	return output
 
 def main():
 	"""Checks that things work"""
